4b43febc7bdd5f3ccf3f4db49a10fbd624a1a3de
eusouogoogle <129336124+eusouogoogle@users.noreply.github.com>
2023-04-19 18:41:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,79 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = ""; 
+    recognition.start();
+} 
+
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+var Content = event.results[0][0].transcript;
+
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+      if(Content =="tire minha selfie")
+      {
+        console.log("tirando selfie --- ");
+        speak();
+      }
+}
+
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    speak_data = "Tirando sua selfie em 5 segundos";
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+
+    setTimeout(function()
+    { 
+        take_snapshot(); 
+        save();
+    }, 5000);
+}
+
+
+
+
+
+
+
+
+ ///LINHA 59 
+camera = document.getElementById("camera");
+//camera: variável
+//document.getElementById; pegar elemento pelo id
+//("camera") id da camera criada no html
+Webcam.set({
+    width:360, //largura 
+    height:250, //altura 
+    image_format : 'jpeg',//formato da imagem 
+    jpeg_quality:90 // qualidade da imagem
+});
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+
+function save()
+{
+  link = document.getElementById("link");
+  image = document.getElementById("selfie_image").src ;
+  link.href = image;
+  link.click();
+}
+
